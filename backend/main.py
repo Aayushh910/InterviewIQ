52ed76d137bea5cdd30797df3b04f9994b072d53
Aayushh910 <aayushsavaliya910@gmail.com>
2026-07-19 10:44:33 +0530

    Complete project structre and files added

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="InterviewIQ API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to InterviewIQ "
+        "Backend"
+    }
